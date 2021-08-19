c8a4354829fd870896c2af582be3a1d13fabb732
offish <overutilization@gmail.com>
2021-08-19 22:18:25 +0200

    Update upload_one.py

--- examples/upload_one.py
+++ examples/upload_one.py
@@ -1,21 +1,21 @@
 from opplast import Upload
 
 
-upload = Upload(
-    "C:/Users/USERNAME/AppData/Roaming/Mozilla/Firefox/Profiles/random.selenium",
-)
+if __name__ == "__main__":
+    upload = Upload(
+        # use r"" for paths, this will not give formatting errors e.g. "\n"
+        r"C:/Users/USERNAME/AppData/Roaming/Mozilla/Firefox/Profiles/r4Nd0m.selenium",
+    )
 
-was_uploaded, video_id = upload.upload(
-    {
-        "file": "path/to/video.mp4",
-        "title": "My YouTube Title",
-        "description": "My YouTube Description",
-        "thumbnail": "path/to/thumbnail.jpg",
-        "tags": ["these", "are", "my", "tags"],
-    }
-)
+    was_uploaded, video_id = upload.upload(
+        r"C:/path/to/video.mp4",
+        title="My YouTube Title",
+        description="My YouTube Description",
+        thumbnail=r"C:/path/to/thumbnail.jpg",
+        tags=["these", "are", "my", "tags"],
+    )
 
-if was_uploaded:
-    print(f"{video_id} has been uploaded to YouTube")
+    if was_uploaded:
+        print(f"{video_id} has been uploaded to YouTube")
 
-upload.close()
+    upload.close()
